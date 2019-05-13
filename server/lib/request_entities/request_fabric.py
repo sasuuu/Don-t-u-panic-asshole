b3116e633a5154f07fae8432190da13a85314da9
sasuuu <37144833+sasuuu@users.noreply.github.com>
2019-05-14 01:41:23 +0200

    Develop pull

--- server/lib/request_entities/request_fabric.py
+++ server/lib/request_entities/request_fabric.py
@@ -1,18 +1,52 @@
+from abc import ABC, abstractmethod
+
 from lib import request_types
 
 
-def return_login(data, connection):
-    return {"request_type": request_types.LOGIN_RESULT, "response": 'True'}
-
-
-def return_servers(data, connection):
-    names = ["Server Krzemień", "Server Kulig", "Server Merta", "Server Kwilosz", "Server Krzystanek",
-             "Server Łyś", "Server Król"]
-    return {"request_type": request_types.SERVER_LISTS, "response": names}
-
-
-def get_request_dictionary():
+def get_request_dictionary(db):
     return {
-        request_types.LOGIN: (lambda data, connection: return_login(data, connection)),
-        request_types.GET_SERVERS: (lambda data, connection: return_servers(data, connection)),
+        request_types.LOGIN: LoginRequestHandler(db),
+        request_types.GET_SERVERS: ServerList(db),
     }
+
+
+class RequestHandler(ABC):
+
+    def __init__(self, db):
+        self._DB = db
+
+    @abstractmethod
+    def handle(self, data, client):
+        pass
+
+
+class LoginRequestHandler(RequestHandler):
+
+    def handle(self, data, client):
+        user = data['username']
+        password = data['password']
+        (result,) = \
+            self._DB.make_query(f"SELECT count(*) FROM player where password = '{password}' and nick='{user}'")[0]
+        if result == 1:
+            print('user ' + user + '  log in')
+            client.login(user)
+            return {"request_type": request_types.LOGIN_RESULT, "response": 'True'}
+        elif result == 0:
+            print('user ' + user + '  fail log in')
+            return {"request_type": request_types.LOGIN_RESULT, "response": 'False'}
+        else:
+            raise ImportError('data base return inconsistency result')
+
+
+class ServerList(RequestHandler):
+
+    def handle(self, data, client):
+        if client.is_logged():
+            server_list = self._DB.make_query(f"SELECT name, server_ip, player_count FROM server")
+            return {"request_type": request_types.SERVER_LISTS, "response": server_list}
+        else:
+            raise IllegalAccessException
+
+
+class IllegalAccessException(Exception):
+    pass
