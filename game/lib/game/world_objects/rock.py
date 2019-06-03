fd104c7dbedcf5c18a40d92816643eae6a0f6677
Magly97 <marcinlys97@gmail.com>
2019-06-03 21:53:16 +0200

    rename and add to json

--- game/lib/game/world_objects/rock.py
+++ game/lib/game/world_objects/rock.py
@@ -1,3 +1,5 @@
+import json
+import os
 import pygame
 from lib.game.world_objects.world_object import WorldObject
 from random import randint
@@ -6,6 +8,12 @@ rock_sprite = [pygame.image.load('config/assets/objects/Kamien1.png'),
                pygame.image.load('config/assets/objects/Kamien2.png'),
                pygame.image.load('config/assets/objects/Kamien3.png'),
                pygame.image.load('config/assets/objects/Kamien4.png')]
+
+object_config = None
+file_exists = os.path.isfile("config/object_config.json")
+if file_exists:
+    with open("config/object_config.json") as json_file:
+        object_config = json.load(json_file)
 
 
 class Rock(WorldObject):
@@ -17,31 +25,39 @@ class Rock(WorldObject):
         self._y_coordinate = y_coordinate
         self._width = width
         self._height = height
-        self._mov_x = 0
-        self._mov_y = 0
-        self.__rand = randint(0, 3)
-        self._sprite_path = rock_sprite[self.__rand]
+        self._width_collision = width
+        self._height_collision = height
+        self._move_left_corner_x = 0
+        self._move_left_corner_y = 0
+        self.__rand_sprite = randint(0, 3)
+        self._sprite_path = rock_sprite[self.__rand_sprite]
 
-        if self.__rand == 1:
-            self._mov_y = 50  # 15  # Y
-            self._mov_x = 20  # 20  # X
-            self._width = 155  # 160  # X
-            self._height = 130  # 165  # Y
-        elif self.__rand == 2:
-            self._mov_y = 70    # 30   # Y
-            self._mov_x = 40    # 20   # X
-            self._width = 140   # 160  # X
-            self._height = 90   # 140  # Y
-        elif self.__rand == 3:
-            self._mov_y = 70  # 30      # Y
-            self._mov_x = 20  # 20      # X
-            self._width = 155  # 155    # X
-            self._height = 90  # 140   # Y
-        elif self.__rand == 0:
-            self._mov_y = 15     # Y
-            self._mov_x = 15     # X
-            self._width = 70    # X
-            self._height = 70   # Y
+        if self.__rand_sprite == 0:
+            self._move_left_corner_y = object_config['rock1_move_left_corner_y']
+            self._move_left_corner_x = object_config['rock1_move_left_corner_x']
+            self._width_collision = object_config['rock1_width_collision']
+            self._height_collision = object_config['rock1_height_collision']
+
+        elif self.__rand_sprite == 1:
+            self._move_left_corner_y = object_config['rock2_move_left_corner_y']
+            self._move_left_corner_x = object_config['rock2_move_left_corner_x']
+            self._width_collision = object_config['rock2_width_collision']
+            self._height_collision = object_config['rock2_height_collision']
+
+        elif self.__rand_sprite == 2:
+            self._move_left_corner_y = object_config['rock3_move_left_corner_y']
+            self._move_left_corner_x = object_config['rock3_move_left_corner_x']
+            self._width_collision = object_config['rock3_width_collision']
+            self._height_collision = object_config['rock3_height_collision']
+
+        elif self.__rand_sprite == 3:
+            self._move_left_corner_y = object_config['rock4_move_left_corner_y']
+            self._move_left_corner_x = object_config['rock4_move_left_corner_x']
+            self._width_collision = object_config['rock4_width_collision']
+            self._height_collision = object_config['rock4_height_collision']
+
+
+
 
 
 
--- game/lib/game/world_objects/tree.py
+++ game/lib/game/world_objects/tree.py
@@ -1,3 +1,6 @@
+import json
+import os
+
 import pygame
 from lib.game.world_objects.world_object import WorldObject
 from random import randint
@@ -9,6 +12,13 @@ tree_sprite = [pygame.image.load('config/assets/objects/Drzewo1.png'),
                pygame.image.load('config/assets/objects/Drzewo4.png')]
 
 
+object_config = None
+file_exists = os.path.isfile("config/object_config.json")
+if file_exists:
+    with open("config/object_config.json") as json_file:
+        object_config = json.load(json_file)
+
+
 class Tree(WorldObject):
 
     def __init__(self, x_coordinate=100, y_coordinate=100, width=70, height=70):
@@ -16,30 +26,37 @@ class Tree(WorldObject):
         self.__real_x = x_coordinate
         self.__real_y = y_coordinate
         self._y_coordinate = y_coordinate
+        self._width_collision = width
+        self._height_collision = height
         self._width = width
         self._height = height
-        self.__rand = randint(0, 3)
-        self._sprite_path = tree_sprite[self.__rand]
-        self._mov_x = 125
-        self._mov_y = 125
+        self.__rand_sprite = randint(0, 3)
+        self._sprite_path = tree_sprite[self.__rand_sprite]
+        self._move_left_corner_x = 125
+        self._move_left_corner_y = 125
 
-        if self.__rand == 1:
-            self._mov_y = 100  # Y
-            self._mov_x = 70  # X
-            self._width = 100  # X
-            self._height = 80  # Y
-        elif self.__rand == 2:
-            self._mov_y = 140   # Y
-            self._mov_x = 140   # X
-            self._width = 40  # X
-            self._height = 40  # Y
-        elif self.__rand == 3:
-            self._mov_y = 130      # Y
-            self._mov_x = 130      # X
-            self._width = 40    # X
-            self._height = 40   # Y
-        elif self.__rand == 0:
-            self._mov_y = 150    # Y
-            self._mov_x = 125     # X
-            self._width = 40    # X
-            self._height = 40   # Y
+        if self.__rand_sprite == 0:
+            self._move_left_corner_y = object_config['tree1_move_left_corner_y']
+            self._move_left_corner_x = object_config['tree1_move_left_corner_x']
+            self._width_collision = object_config['tree1_width_collision']
+            self._height_collision = object_config['tree1_height_collision']
+
+        elif self.__rand_sprite == 1:
+            self._move_left_corner_y = object_config['tree2_move_left_corner_y']
+            self._move_left_corner_x = object_config['tree2_move_left_corner_x']
+            self._width_collision = object_config['tree2_width_collision']
+            self._height_collision = object_config['tree2_height_collision']
+
+        elif self.__rand_sprite == 2:
+            self._move_left_corner_y = object_config['tree3_move_left_corner_y']
+            self._move_left_corner_x = object_config['tree3_move_left_corner_x']
+            self._width_collision = object_config['tree3_width_collision']
+            self._height_collision = object_config['tree3_height_collision']
+
+        elif self.__rand_sprite == 3:
+            self._move_left_corner_y = object_config['tree4_move_left_corner_y']
+            self._move_left_corner_x = object_config['tree4_move_left_corner_x']
+            self._width_collision = object_config['tree4_width_collision']
+            self._height_collision = object_config['tree4_height_collision']
+
+
