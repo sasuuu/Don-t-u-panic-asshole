a9a81dc83c53754adf477a67fabdd119d25a3d37
sasuuu <37144833+sasuuu@users.noreply.github.com>
2019-06-11 00:46:53 +0200

    Small fix

--- server_UDP/lib/model/character.py
+++ server_UDP/lib/model/character.py
@@ -5,7 +5,7 @@ from lib.model.game_object import GameObject
 class Character(GameObject):
 
     def __init__(self, idx, nick, health, position, items):
-        super(Character, self).__init__(idx, position, ObjectTypes.CHARACTER)
+        super(Character, self).__init__(idx, position, ObjectTypes.CHARACTER.value)
         self.nick = nick
         self.health = health
         self.items = items
