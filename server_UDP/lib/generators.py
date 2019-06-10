9206c86f51834ce5f81a33d963e30e0f92479508
kpv16 <kpv135@gmail.com>
2019-06-10 21:42:22 +0200

    small name fix

--- server_UDP/lib/generators.py
+++ server_UDP/lib/generators.py
@@ -1,15 +1,25 @@
 import random
 
-from lib.model.water import Water
+from lib.object_types import *
+from lib.model.game_object import GameObject
 
-MIN_WATER_PLACE = 5
-MAX_WATER_PLACE = 100
+MIN_WATER_PLACE_COUNT = 5
+MAX_WATER_PLACE_COUNT = 100
+
+MIN_ITEMS_COUNT = 10
+MAX_ITEMS_COUNT = 11
+
+MAP_HEIGHT = 50000
+MAP_WIDTH = 50000
+
 
 class MapGenerator(object):
     @staticmethod
     def generate_objects_list():
         list = []
-        for i in range(random.randrange(MIN_WATER_PLACE,MAX_WATER_PLACE)):
-            list.append(Water())
+        for i in range(random.randrange(MIN_ITEMS_COUNT, MAX_ITEMS_COUNT)):
+            list.append(GameObject(GameObject.get_next_id(),
+                                   (random.randrange(-MAP_WIDTH, MAP_WIDTH), random.randrange(-MAP_HEIGHT, MAP_HEIGHT)),
+                                   random.randrange(1, OBJECT_TYPES_COUNT)))
 
         return list
--- server_UDP/lib/model/water.py
+++
@@ -1,7 +0,0 @@
-from lib.model.game_object import GameObject
-
-
-class Water(GameObject):
-
-    def __init__(self, idx, position, object_type):
-        super(Water, self).__init__(idx, position, object_type)
