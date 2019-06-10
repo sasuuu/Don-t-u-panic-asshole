88829b3f70d96bb8ea69d0d4b9d5912b863b2893
kpv16 <kpv135@gmail.com>
2019-06-10 21:43:07 +0200

    add map generator and sing object to chunk

--- server_UDP/UDP_server_start.py
+++ server_UDP/UDP_server_start.py
@@ -3,6 +3,7 @@ from lib.server import Server
 if __name__ == '__main__':
     server = Server()
     server.start()
+
     input("Press enter to exit ;)\n")
     print("DEBUG send signal to stop sever")
     server.stop()
